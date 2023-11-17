38be2cfeb917cd2f284d29f59aa0cf96edb141da
Wanjohi <71614375+wanjohiryan@users.noreply.github.com>
2023-11-17 09:16:44 +0300

    Fix directory issues

--- .docker/wine/entrypoint.sh
+++ .docker/wine/entrypoint.sh
@@ -8,5 +8,6 @@ echo "Waiting for X server"
 until [[ -e /var/run/appconfig/xserver_ready ]]; do sleep 1; done
 echo "X server is ready"
 
+#TODO: explicitly set the wine prefix
 # Start Wine application
-wine "$@"
+wine /game/"$@"
